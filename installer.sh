3ae36df2012f7b4b42d18b803f2bb536d37ea7fe
emil-nabil333 <elbrinselomda@yahoo.com>
2025-11-30 15:49:08 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#
+# Command:
+# wget https://raw.githubusercontent.com/emilnabil/channel-abo-naser/main/installer.sh -qO - | /bin/sh
+#
+# ###########################################################
+
+MY_URL="https://raw.githubusercontent.com/emilnabil/channel-abo-naser/main"
+
+echo "******************************************************************************************************************"
+echo "        DOWNLOAD AND INSTALL CHANNEL"
+echo "=================================================================================================================="
+if [ -f /etc/opkg/opkg.conf ]; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+fi
+
+echo "        REMOVE OLD CHANNELS..."
+
+rm -rf /etc/enigma2/lamedb
+rm -rf /etc/enigma2/*list
+rm -rf /etc/enigma2/*.tv
+rm -rf /etc/enigma2/*.radio
+
+#####################################################################################
+echo "        INSTALLING NEW CHANNELS..."
+cd /tmp
+set -e
+wget -q "${MY_URL}/channels_backup_by_abo-naser.tar.gz"
+tar -xzf channels_backup_by_abo-naser.tar.gz -C /
+cd ..
+set +e
+rm -f /tmp/channels_backup_by_abo-naser.tar.gz
+sleep 2
+
+echo ""
+echo "        INSTALLING ASTRA-SM PATCH"
+opkg install astra-sm -y >/dev/null 2>&1
+sleep 1
+echo ""
+echo ""
+echo "****************************************************************************************************************************"
+echo "#       CHANNEL INSTALLED SUCCESSFULLY       #"
+echo "*********************************************************"
+echo "********************************************************************************"
+echo "   UPLOADED BY >>>> EMIL_NABIL"
+sleep 4
+echo "========================================================================================================================="
+echo "        >>>> RESTARTING <<<<"
+echo "**********************************************************************************"
+
+if [ "${OSTYPE}" = "Opensource" ]; then
+    killall -9 enigma2
+else
+    systemctl restart enigma2
+fi
+
+exit 0
+
+
+
+
+
+
